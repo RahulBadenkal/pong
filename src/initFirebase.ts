918cc1859c190505e7c7aa521dd7830df49be152
Rahul <rahulbadenkal@gmail.com>
2021-12-30 13:52:22 +0530

    temp

---
+++ src/initFirebase.ts
@@ -0,0 +1,28 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2x7zDAsz5zoC6jOoqrpnP7PRLYOtZk9s",
+  authDomain: "pong-e692c.firebaseapp.com",
+  projectId: "pong-e692c",
+  storageBucket: "pong-e692c.appspot.com",
+  messagingSenderId: "678084788906",
+  appId: "1:678084788906:web:e5b4c82be06edd6eccc6d3",
+  measurementId: "G-Y4DJ1W4J6T"
+};
+
+export async function initFirebase() {
+  if (typeof window !== "undefined") {
+    // You need to use firebase/app for the client side
+    const fb = (await import("firebase/app")).default;
+    await import("firebase/auth");
+    await import("firebase/firestore");
+    fb.initializeApp(firebaseConfig);
+    return;
+  }
+
+  const fb: any = await import("firebase");
+  if (fb.apps.length == 0) {
+    fb.initializeApp(firebaseConfig);
+    return;
+  }
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script lang="ts">
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "@firebase/app";
+import { getAnalytics } from "@firebase/analytics";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, user => {
+  if (user === null) {
+    console.log("No user");
+  }
+  else {
+    console.log(`${user} logged in`);
+  }
+})
+</script>
