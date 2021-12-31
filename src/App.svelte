552e91ea6e60fdcab9dc140704c5d8933196b2c8
Rahul <rahulbadenkal@gmail.com>
2022-01-01 00:13:48 +0530

    added all except pixi

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
   import Tailwind from "./Tailwind.svelte";
-
-
-	export let name: string;
 </script>
 
 <Tailwind />
-<div class="bg-gray-200 px-4 py-2 rounded">Hello Taasdsadilwind!</div>
+<div class="bg-gray-200 px-4 py-2 rounded">Hello Tailwind!</div>
 
 
 <style>
