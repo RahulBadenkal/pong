861fb80e8fa8f6f45b2bf9481c6dc116cc18db6f
Rahul <rahulbadenkal@gmail.com>
2021-12-31 23:42:45 +0530

    Added svelte-typescript-tailwind

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Tailwind from "./Tailwind.svelte";
+
+
+	export let name: string;
+</script>
+
+<Tailwind />
+<div class="bg-gray-200 px-4 py-2 rounded">Hello Taasdsadilwind!</div>
+
+
+<style>
+
+</style>
