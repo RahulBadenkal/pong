cbd1eb8c5ea35cb8fc6bc2d686f4a2a577073b6d
Rahul <rahulbadenkal@gmail.com>
2022-01-25 01:00:16 +0530

    glitchy animation

--- src/App.svelte
+++ src/App.svelte
@@ -4,36 +4,38 @@
 
   import { Application, Graphics, Text, TextStyle } from 'pixi.js';
 
+  // All velocities are in px/second
   type Game = {
+    fps: number,
     pixiApp: Application;
-    geometry?: {
+    geometry: {
       container: { width: number; height: number };
       verticalPaddingSize: number; dividerSize: number; padding: number; padWidth: number; padHeight: number;
-      fontSize: number; yOffset: number; xOffset: number; stepMovement: number
+      fontSize: number; yOffset: number; xOffset: number; padSpeed: number, ballRadius: number,
       startYPoint?: number; endYPoint?: number;
     };
+    lastFrameTimeMs: number;
     colors: { lightGray: number }
     keyMap: {[key in string]: boolean};
-    player1?: { y: number };
-    player2?: { y: number };
-    ball?: { x: number, y: number }
+    player1?: { position: { x: number, y: number }; velocity: { x: number; y: number } };
+    player2?: { position: { x: number, y: number }; velocity: { x: number; y: number } };
+    ball?: { position: { x: number; y: number }, velocity: { x: number; y: number }}
   }
 
   const setup = (): Game => {
     const game: Game = {
+      fps: 60,
       pixiApp: null,
       geometry: { container: { width: 640, height: 480 }, 
-        verticalPaddingSize: 15, dividerSize: 15, padding: 20, padWidth: 15, padHeight: 100,
-        fontSize: 56, yOffset: 20, xOffset: 48, stepMovement: 10,
+        verticalPaddingSize: 15, dividerSize: 15, padding: 20, padWidth: 15, padHeight: 80,
+        fontSize: 56, yOffset: 20, xOffset: 48, padSpeed: 0.4, ballRadius: 8,
       },
+      lastFrameTimeMs: Date.now(),
       colors: { lightGray: 0xe2e8f0 },
       keyMap: {},
-      player1: {
-        y:  480/2 - 100/2 // height/2 - padHeight/2, 
-      },
-      player2: { 
-        y:  480/2 - 100/2 // height/2 - padHeight/2, 
-      }
+      player1: { position: { x: 20, y: 480/2 }, velocity: { x: 0, y: 0.4 } },  // x: padding
+      player2: { position: { x: 640 - 20 - 15, y: 480/2 }, velocity: { x: 0, y: 0.4 } },  // x: WIDTH - padding - padWidth,
+      ball: { position: { x: 640/2, y: 480/2 }, velocity: { x: 0.1, y: 0 } }
     }
 
     // Add keyboard listeners
@@ -109,24 +111,139 @@
   }
 
   const update = (game: Game) => {
-    const { geometry, player1, player2, keyMap } = game
-    const { container, verticalPaddingSize, padHeight, stepMovement } = geometry
-    const { height: HEIGHT } = container
+    const { geometry, player1, player2, ball, keyMap, lastFrameTimeMs } = game
+    const { container, verticalPaddingSize, padWidth, padHeight, padSpeed, ballRadius } = geometry
+    const { height: HEIGHT, width: WIDTH } = container
+    game.lastFrameTimeMs = Date.now()
+    const delta = game.lastFrameTimeMs - lastFrameTimeMs
 
-    if (keyMap["ArrowUp"] === true) {
-      player2.y = Math.max(0 + verticalPaddingSize, player2.y - stepMovement); 
+    // Determine player 2 velocity by reding user input 
+    // Only one key is processed at any time
+    if (keyMap["ArrowUp"]) {
+      player2.velocity = { x: 0, y: -1 * padSpeed }
     }
-    else if (keyMap["ArrowDown"] === false) {
-      player2.y = Math.min(HEIGHT - verticalPaddingSize - padHeight, player2.y + stepMovement)
+    else if (keyMap["ArrowDown"]) {
+      player2.velocity = { x: 0, y: 1 * padSpeed }
+    }
+    else {
+      player2.velocity = { x: 0, y: 0 }
     }
 
-    return game
+    // Determine player 1 (AI logic)
+    player1.velocity = { x: 0, y: 0 }
+
+    // Calculate player 1 pad position
+    player1.position = { 
+      x: player1.position.x + player1.velocity.x * delta, 
+      y: player1.position.y + player1.velocity.y * delta 
+    }
+
+    // Calculate player 2 pad position
+    player2.position = { 
+      x: player2.position.x + player2.velocity.x * delta, 
+      y: player2.position.y + player2.velocity.y * delta 
+    }
+
+    // Calculate ball position
+    ball.position = {
+      x: ball.position.x + ball.velocity.x * delta, 
+      y: ball.position.y + ball.velocity.y * delta
+    }
+
+    // Handle collision
+    const _handlePadCollisionWithWall = (position: { x: number, y: number }) => {
+      position.y = Math.max(0 + verticalPaddingSize, position.y)
+      position.y = Math.min(HEIGHT - verticalPaddingSize - padHeight, position.y)
+      return position
+    }
+
+    const _isNumBetween = (num: number, num1: number, num2: number) => {
+      return num >= num1 && num <= num2
+    }
+
+    const _doBoundingBoxCollide = (obj1: { x1: number; x2: number; y1: number; y2: number }, obj2: { x1: number; x2: number; y1: number, y2: number}) => {
+      [obj1, obj2] = obj1.x1 <= obj2.x1 ? [obj1, obj2] : [obj2, obj1];
+      const collideInX = _isNumBetween(obj2.x1, obj1.x1, obj1.x2) ||_isNumBetween(obj2.x2, obj1.x1, obj1.x2);
+
+      [obj1, obj2] = obj1.y1 <= obj2.y1 ? [obj1, obj2] : [obj2, obj1];
+      const collideInY = _isNumBetween(obj2.y1, obj1.y1, obj1.y2) ||_isNumBetween(obj2.y2, obj1.y1, obj1.y2);
+
+      return collideInX && collideInY
+    }
+
+    const _getBallBoundingBox = (ballRadius: number, position: { x: number; y: number }) => {
+      return {
+        x1: position.x - ballRadius, x2: position.x + ballRadius,
+        y1: position.y - ballRadius, y2: position.y + ballRadius
+      }
+    }
+
+    const _getPadBoundingBox = (padWidth: number, padHeight: number, position: { x: number; y: number }) => {
+      return {
+        x1: position.x, x2: position.x + padWidth,
+        y1: position.y, y2: position.y + padHeight
+      }
+    }
+
+    const _isBallCollidingWithPad = (ballRadius: number, padWidth: number, padHeight: number, ballPostion: { x: number; y: number }, padPosition: {x: number; y: number }) => {
+      return _doBoundingBoxCollide(
+        _getBallBoundingBox(ballRadius, ballPostion), 
+        _getPadBoundingBox(padWidth, padHeight, padPosition)
+        )
+    }
+
+    const _handleBallCollisionWithPad = (ballRadius: number, padWidth: number, padHeight: number, ball: { position: { x: number; y: number }, velocity: { x: number; y: number } }, pad: { position: { x: number; y: number }, velocity: {x: number; y: number }}) => {
+      if (_isBallCollidingWithPad(ballRadius, padWidth, padHeight, ball.position, pad.position)) {
+        return { x: -1 * ball.velocity.x, y: ball.velocity.y + pad.velocity.y }
+      }
+      else {
+        return ball.velocity
+      }
+    }
+
+    const _handleBallCollisionWithWall = (ballRadius: number, ball: { position: { x: number; y: number }, velocity: { x: number; y: number }}) => {
+      const ballBoundingBox = _getBallBoundingBox(ballRadius, ball.position)
+      const initialBallConfig = {
+        position: { x: WIDTH / 2, y: HEIGHT / 2 },
+        velocity: { x: 0.4, y: 0 },
+      }
+      
+      // Horizontal collision
+      if (ballBoundingBox.x1 <= 0 || ballBoundingBox.x2 >= WIDTH) {
+        return initialBallConfig
+      }
+
+      // Vertical collision
+      if (ballBoundingBox.y1 <= 0 || ballBoundingBox.y2 >= HEIGHT) {
+        return {
+          position: ball.position,
+          velocity: { x: ball.velocity.x, y: -1 * ball.velocity.y }
+        }
+      }
+
+      return ball
+    }
+
+    // Pad collision with surface
+    player1.position = _handlePadCollisionWithWall(player1.position)
+    player2.position = _handlePadCollisionWithWall(player2.position)
+
+    // Ball collision with pad
+    ball.velocity = _handleBallCollisionWithPad(ballRadius, padWidth, padHeight, ball, player1)
+    ball.velocity = _handleBallCollisionWithPad(ballRadius, padWidth, padHeight, ball, player2)
+
+    // Ball collision with wall
+    let newBall = _handleBallCollisionWithWall(ballRadius, ball)
+    ball.position = newBall.position;
+    ball.velocity = newBall.velocity
+
+    return { ...game, player1, player2, ball }
   }
 
   // This changes frequently
   const renderGameScene = (game: Game): Game => {
-    const { pixiApp: app, colors, geometry, player1: player1Setting, player2: player2Setting } = game
-    const { container, padding, padWidth, padHeight, startYPoint, fontSize, xOffset, yOffset } = geometry
+    const { pixiApp: app, colors, geometry, player1: player1Setting, player2: player2Setting, ball: ballSetting } = game
+    const { container, padding, padWidth, padHeight, startYPoint, fontSize, xOffset, yOffset, ballRadius } = geometry
     const { width: WIDTH, height: HEIGHT } = container
     const { lightGray } = colors
 
@@ -134,7 +251,7 @@
       let graphicsObj = app.stage.getChildByName(name) as Graphics
       if (!graphicsObj) {
         graphicsObj = new Graphics()
-        graphicsObj.name = "pads"
+        graphicsObj.name = name
       }
       else {
         graphicsObj.clear()
@@ -147,8 +264,8 @@
     // Add player 1 pad
     pads.beginFill(lightGray);
     pads.drawRect(
-        padding,
-        player1Setting.y,
+        player1Setting.position.x,
+        player1Setting.position.y,
         padWidth,
         padHeight,
     );
@@ -156,8 +273,8 @@
     // Add player 2 pad
     pads.beginFill(lightGray);
     pads.drawRect(
-        WIDTH - padding - padWidth,
-        player2Setting.y,
+        player2Setting.position.x,
+        player2Setting.position.y,
         padWidth,
         padHeight,
     );
@@ -166,9 +283,9 @@
     app.stage.addChild(pads);
 
     /* Ball */
-    const ball = new Graphics()
+    const ball = _getGraphicsObj(app, "ball")
     ball.beginFill(lightGray);
-    ball.drawCircle(WIDTH/2, HEIGHT/2, 8)
+    ball.drawCircle(ballSetting.position.x, ballSetting.position.y, ballRadius)
     ball.endFill()
     app.stage.addChild(ball)
 
@@ -190,20 +307,14 @@
   }
 
   const downListener = (event: KeyboardEvent, keyMap) => {
-    console.log("keyDown", event.key)
     event.preventDefault()
-    if (!keyMap.hasOwnProperty(event.key)) keyMap[event.key] = false  // Keydown pressed for the first time
-    else if (keyMap[event.key])  delete keyMap[event.key]  // keydown pressed from releasing keyup
-    else keyMap[event.key] = false
+    keyMap[event.key] = true
     return keyMap
   }
 
   const upListener = (event: KeyboardEvent, keyMap) => {
-    console.log("keyUp", event.key)
     event.preventDefault()
-    if (!keyMap.hasOwnProperty(event.key)) keyMap[event.key] = true  // keyup pressed for the first time
-    else if (!keyMap[event.key])  delete keyMap[event.key]  // keyup pressed from releasing keydown
-    else keyMap[event.key] = true
+    keyMap[event.key] = false
     return keyMap
   }
 
@@ -216,7 +327,7 @@
     setInterval(() => {
       game = update(game)
       game = renderGameScene(game)
-    }, 1000 / 60)
+    }, 1000 / game.fps )
 
   };
 
